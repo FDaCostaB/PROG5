2eb064de7ba8df7830cdb6eb17c8696b1ae1193c
Hussein Fakharany <fakharany.hussein@gmail.com>
2021-01-06 19:10:52 +0200

    data proc v1

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -29,9 +29,89 @@ Contact: Guillaume.Huard@imag.fr
 
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
+	uint8_t opcode = (uint8_t)((ins & 0xf00000) >> 20);
+	switch (opcode){
+		case 0:
+			arm_and(p,ins);
+			break;
+		case 1:
+			arm_eor(p,ins);
+			break;
+		case 2:
+			arm_sub(p,ins);
+			break;
+		case 3:
+			arm_rsb(p,ins);
+			break;
+		case 4:
+			arm_add(p,ins);
+			break;
+		case 5:
+			arm_adc(p,ins);
+			break;
+		case 6:
+			arm_sbc(p,ins);
+			break;
+		case 7:
+			arm_rsc(p,ins);
+			break;
+		case 8:
+			arm_tst(p,ins);
+			break;
+		case 9:
+			arm_teq(p,ins);
+			break;
+		case 10:
+			arm_cmp(p,ins);
+			break;
+		case 11:
+			arm_cmn(p,ins);
+			break;
+		case 12:
+			arm_orr(p,ins);
+			break;
+		case 13:
+			arm_mov(p,ins);
+			break;
+		case 14:
+			arm_bic(p,ins);
+			break;
+		case 15:
+			arm_mvn(p,ins);
+			break;
+	}
     return UNDEFINED_INSTRUCTION;
 }
 
+int shifter_operand(arm_core p, uint32_t ins,uint32_t (*fonction)(uint32_t val1,uint32_t val2) ){
+	uint32_t rn = (ins << (sizeof(ins) - 19)) >> 28; //adresse registre source
+	uint32_t rd = (ins << (sizeof(ins) - 15)) >> 28; //adresse registre destination
+	uint32_t operateur = (ins << sizeof(ins) -20) >> 20; //I
+	uint32_t shOpAdd = (ins & 0xfff); //bits shifter operand
+	uint32_t val1;
+	uint32_t val2;
+	uint32_t res;
+
+	if (operateur==1){ //valeur immediate 
+		val2 = shOpAdd & 0xff;
+		memory_read_word(p->mem,rn,&val1);
+		res = fonction(val1,val2); //!! rotate_imm ??
+		arm_write_register(p,rd,res);
+	} else {
+		if (shOpAdd <= 15){//shifter_operand = Rm
+			uint32_t rm = (shOpAdd & 0xf);
+			memory_read_word(p->mem,rn,&val1);
+			memory_read_word(p->mem,rm,&val2);
+			res = val1+val2;
+			arm_write_register(p,rd,res);
+		}
+		else if {
+
+		}
+	
+	}
+
+}
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
 }
