ae5956b9cab5f8cc4ef322fd94158e98fbf03e48
root <root@LAPTOP-RFL73B52.localdomain>
2021-01-07 10:05:24 +0100

    v2 de arm_instruction

--- arm_instruction.c
+++ arm_instruction.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -29,39 +29,44 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 
 static int arm_execute_instruction(arm_core p) {
-    uint32_t ins;
-    arm_fetch(p,&ins);
-    printf("%08x \n",ins);
-    uint8_t champ = (uint8_t)((ins & 0xe000000) >> 25);
-    printf("%d \n",champ);
-    switch (champ) {
-        case 0:
-        case 1:
-            arm_data_processing_shift(p, ins);
-            break;
-        case 2:
-
-            break;
-        case 3:
-
-            break;
-        case 4:
-
-            break;
-        case 5:
-            arm_branch(p, ins);
-            break;
-        case 6:
-
-            break;
-        case 7:
-
-            break;
-        default:
-            return 1;
-            break;
+    uint32_t instruction;
+    if(arm_fetch(p, &instruction)){
+        exit(EXIT_FAILURE).
     }
-
+    else{
+        switch(get_bits(instruction, 27, 25))
+        {
+            case 0b000:
+                /* miscellaneous*/
+                break;
+            case 0b001:
+                        /* Move immediate to status register */
+                        /* Undefined instruction  */
+                        /* Data processing immediate */
+                break;
+            case 0b010:
+                arm_load_store(p, instruction);
+                break;
+            case 0b011:
+                    /* Media instructions */
+                    /* Load/store register offset */
+                break;
+            case 0b100:
+                arm_load_store_multiple(p, instruction);
+                break;
+            case 0b101:
+                arm_branch(p, instruction);
+                break;
+            case 0b110:
+                arm_coprocessor_load_store(p, instruction);
+                break;
+            case 0b111:
+                arm_coprocessor_others_swi(p, instruction);
+                break;
+            default:
+                break;
+        }
+    }
     return 0;
 }
 
